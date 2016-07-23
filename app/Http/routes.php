74b681c66f0aa5d5f0d2afd14b26ff881695ebff
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2016-07-23 23:18:31 +0600

    Authentication system

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,13 @@
 |
 */
 
+Route::get('auth/login', ['as' => 'login', 'uses' => 'Auth\AuthController@getLogin']);
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', ['as' => 'logout', 'uses' => 'Auth\AuthController@getLogout']);
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogController@getIndex']);
 Route::get('/', 'PagesController@getIndex');
